08ca02326dceb0529604c4ec88f916a16fe0dfa8
kinkcrafter <65737354+kinkcrafter@users.noreply.github.com>
2020-05-21 22:47:23 +0000

    project files and startup c#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace MLTest
+{
+    class Program
+    {
+        static IDisposable server;
+
+        static void Main(string[] args)
+        {
+            InitializeOwin();
+
+            Console.WriteLine("Press any Key to close!");
+            Console.ReadKey();
+        }
+
+
+        private static void InitializeOwin()
+        {
+            server = WebApp.Start<Startup>("http://*:12346");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MLTest
+
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            string strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string exePath = Path.GetDirectoryName(strExeFilePath);
+
+
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            app.UseWebApi(config);
+
+#if DEBUG
+            exePath = Directory.GetParent(Directory.GetParent(exePath).FullName).FullName; 
+#endif
+            var physicalFileSystem = new PhysicalFileSystem(exePath + "\\Web");
+
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem,
+                EnableDirectoryBrowsing = true
+            };
+            options.StaticFileOptions.FileSystem = physicalFileSystem;
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+            options.DefaultFilesOptions.DefaultFileNames = new[] { "index.html" };
+            app.UseFileServer(options);
+        }
+    }
+}
